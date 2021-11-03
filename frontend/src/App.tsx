69857cc600ef4d90f13e0cd7132e2d2aa9146977
Marcio <marciogarrido@id.uff.br>
2021-11-02 22:55:49 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
+
+import Datatable from 'components/Datatable';
+import Footer from 'components/Footer';
+import Navbar from 'components/Navbar';
 import React from 'react';
 
 
 function App() {
   return (
-    <div>
-      <h1 className="text-primary"> Olá Mundo  </h1> 
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <h1 className="text-primary"> Olá Mundo  </h1>
+       <Datatable/>
+      </div>
+      <Footer/>
+    </>
   );
 }
 
